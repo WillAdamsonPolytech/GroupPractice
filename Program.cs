be5e38642ebc2f5e03537c023e4f66f76c379abe
t4812 <yadatm1@student.op.ac.nz>
2024-05-16 15:34:50 +1200

    this is my code

--- Program.cs
+++ Program.cs
@@ -2,9 +2,92 @@
 {
     internal class Program
     {
+        static void Trevor()
+        {
+            // the variables that i have player position and computer position is the initial spot the player and computer are at 
+            // dial result is the dice being rolled
+            int playerPosition = 0;
+            int computerPosition = 0;
+            int dialResult;
+
+            Console.WriteLine("welcome to snakes and ladders!");
+            Console.WriteLine("first person to reach 100 wins!");
+
+            while (playerPosition < 100 && computerPosition < 100)
+            {
+                Console.WriteLine("players turn press enter to spin the dial...");
+                Console.ReadLine();
+                //dial being spun
+                dialResult = new Random().Next(1, 11);
+
+                //moves player 
+                playerPosition += dialResult;
+
+                //this is the good and bad zones (landing on snake or ladder)
+                if (playerPosition == 25 || playerPosition == 50 || playerPosition == 75)
+                {
+                    Console.WriteLine("you landed on a snake :( move down 10 spaces");
+                    playerPosition -= 10;
+                }
+                else if (playerPosition == 30 || playerPosition == 60 || playerPosition == 90)
+                {
+                    Console.WriteLine("you landed on a ladder :) move up 5 spaces");
+                    playerPosition += 5;
+                }
+
+                //making sure the player doesn't go beyond 100
+                if (playerPosition > 100)
+                    playerPosition = 100;
+                Console.WriteLine($"you are now at {playerPosition} on the board!");
+
+                // checks for winner
+                if (playerPosition == 100)
+                {
+                    Console.WriteLine("congratulations! you reached 100 and won the game! ABSOLUTE CINEMA!!!");
+                    break;
+                }
+
+                Console.WriteLine("computer's turn: press enter for computer to spin the dial...");
+                Console.ReadLine();
+
+                // dial is spinning for the computer
+                dialResult = new Random().Next(1, 11);
+
+                // moves computer
+                computerPosition += dialResult;
+
+                // this is where you find out if the computer landed on a snake or a ladder 
+                if (computerPosition == 25 || computerPosition == 50 || computerPosition == 75)
+                {
+                    Console.WriteLine("computer landed on a snake. moving down 10 spaces.");
+                    computerPosition -= 10;
+                }
+                else if (computerPosition == 30 || computerPosition == 60 || computerPosition == 90)
+                {
+                    Console.WriteLine("computer landed on a ladder. moving up 5 spaces.");
+                    computerPosition += 5;
+                }
+
+                // makes sure that computerPosition does not go above 100
+                if (computerPosition > 100)
+                    computerPosition = 100;
+
+                Console.WriteLine($"computer is now at position {computerPosition}.");
+
+                // checks for winner
+                if (computerPosition == 100)
+                {
+                    Console.WriteLine("computer reached 100 and won the game!");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Trevor();
         }
     }
 }
